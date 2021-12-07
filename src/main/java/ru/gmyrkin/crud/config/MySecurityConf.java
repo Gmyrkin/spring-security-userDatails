5db673cb5ae7514e8f24b0f80553533a5184dc94
Gmyrkin <gmyrkin@gmail.com>
2021-12-07 16:44:24 +0300

    new class and new htps

--- src/main/java/ru/gmyrkin/crud/config/MySecurityConf.java
+++ src/main/java/ru/gmyrkin/crud/config/MySecurityConf.java
@@ -2,20 +2,27 @@ package ru.gmyrkin.crud.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import ru.gmyrkin.crud.services.MyUserDetailsService;
 
+@Configuration
 @EnableWebSecurity
+@ComponentScan
 public class MySecurityConf extends WebSecurityConfigurerAdapter {
-   @Autowired
-   PasswordEncoder passwordEncoder;
+    @Autowired
+    PasswordEncoder passwordEncoder;
 
     @Autowired
     MyUserDetailsService myUserDetailsService;
@@ -37,11 +44,40 @@ public class MySecurityConf extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/").hasAnyRole("ADMIN", "USER")
-                .antMatchers("/user/**").hasRole("ADMIN")
-                .and().formLogin().permitAll()
-        .and().csrf().disable();
+        http
+//                .authorizeRequests()
+//                .antMatchers("/").hasAnyRole("ADMIN", "USER")
+//                .antMatchers("/user/**").hasRole("ADMIN")
+//                .and().formLogin().permitAll()
+//                .and().csrf().disable();
+
+                .formLogin()
+                .loginPage("/login") //указываем логику обработки при логине
+                .usernameParameter("username") // Указываем параметры логина и пароля с формы логина
+                .passwordParameter("password") // Указываем параметры логина и пароля с формы логина
+                .loginProcessingUrl("/perform_login")
+                .successHandler(new LoginSuccessHandler())
+                .permitAll(); // указываем страницу с формой логина
+
+        http // разрешаем делать логаут всем
+                .logout()
+                .permitAll() // указываем URL логаута
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                // указываем URL при удачном логауте
+                .logoutSuccessUrl("/login?logout")
+                .and().csrf().disable();
+
+        http // делаем страницу регистрации недоступной для авторизированных пользователей
+
+                .authorizeRequests()
+                //страницы аутентификаци доступна всем
+                .antMatchers("/login").anonymous()
+                .antMatchers("/user").access("hasAnyRole('ADMIN', 'USER')")
+                .antMatchers("/admin/**").access("hasAnyRole('ADMIN')")
+                // защищенные URL
+                .antMatchers("/hello").access("hasAnyRole('ADMIN')")
+                .anyRequest().authenticated();
+
     }
 
     @Bean
---
+++ src/main/java/ru/gmyrkin/crud/controller/AdminController.java
@@ -0,0 +1,115 @@
+package ru.gmyrkin.crud.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import ru.gmyrkin.crud.model.Role;
+import ru.gmyrkin.crud.model.User;
+import ru.gmyrkin.crud.services.RoleService;
+import ru.gmyrkin.crud.services.UserService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+    private UserService userService;
+    private RoleService roleService;
+@Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+@Autowired
+    public void setRoleService(RoleService roleService) {
+        this.roleService = roleService;
+    }
+
+    @Autowired (required = true)
+   // @Qualifier (value = "userService")
+    public AdminController(UserService userService) {
+        this.userService = userService;
+
+    }
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+//    @GetMapping("/test")
+//    public String testUser (ModelMap model){
+////        model.addAttribute("user", new User(1, "Tim", "123"));
+//        List<User> users = new ArrayList<>();
+//        users.add(new User(23, "Dim", "123"));
+//        users.add(new User(25, "Tim", "456"));
+//        users.add(new User(20, "Rim", "789"));
+//        model.addAttribute("admin", users);
+//
+//        return "test";
+//    }
+
+    @GetMapping(value = "/user")
+    public String listUsers (ModelMap model) {
+       // model.addAttribute("user", new User(1,"Tim","123"));
+        model.addAttribute("listUser",userService.getAllUser());
+
+        return "admin";
+    }
+
+    @PostMapping("/user/add")
+    public String addUser (@ModelAttribute User user){
+        if (user.getId() == 0) {
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
+            Set<Role> roleDef = new HashSet<>();
+            roleDef.add(roleService.getRoleId(2L));
+            user.setRoles(roleDef);
+            userService.addUser(user);
+        } else {
+            userService.updateUser(user);
+        }
+        return "admin";
+    }
+
+    @GetMapping("/user/add")
+    public String addUser (){
+        User user = new User();
+        user.setName("test");
+        user.setPassword(passwordEncoder.encode("test"));
+        userService.addUser(user);
+        return "admin";
+    }
+    @GetMapping("/user/update/{id}")
+    public ModelAndView getUserId (@PathVariable("id") long id){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("update");
+        modelAndView.addObject("userEdit", userService.getUserId(id));
+
+
+        return modelAndView;
+    }
+    @PostMapping("/update/{id}")
+    public String updateUser (@ModelAttribute User user, @PathVariable("id") long id){
+        User userFromDB = userService.getUserId(id);
+        if (userFromDB != null) {
+            userFromDB.setName(user.getName());
+            userFromDB.setPassword(passwordEncoder.encode(user.getPassword()));
+            userService.updateUser(userFromDB);
+        }
+
+        return "admin";
+    }
+
+    @GetMapping ("/remove/{id}")
+    public String deleteUser (@PathVariable("id") long id){
+        User userFind = userService.getUserId(id);
+        if (userFind != null){
+            userService.deleteUser(userFind);
+        }
+
+        return "admin";
+    }
+
+
+}
